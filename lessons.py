31970f26bca3f0ece524bf8a1b73e271fce2245a
Ash2803 <ashot.93@mail.ru>
2023-01-06 01:56:39 +0400

    Fixed message format

--- lessons.py
+++ lessons.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import time
+from textwrap import dedent
 
 import requests
 import telegram
@@ -24,13 +25,15 @@ def get_reviews_status(auth_token, chat_id, bot):
                 'timestamp': timestamp
             }
             if review_status:
-                bot.send_message(chat_id=chat_id, text=f'У вас проверили работу "{lesson_title}"\n'
-                                                       f'К сожалению, в работе нашлись ошибки\n'
-                                                       f'{lesson_url}')
+                bot.send_message(chat_id=chat_id, text=dedent(f'''\
+                У вас проверили работу "{lesson_title}"
+                К сожалению, в работе нашлись ошибки 
+                {lesson_url}'''))
             else:
-                bot.send_message(chat_id=chat_id, text=f'У вас проверили работу "{lesson_title}"\n'
-                                                       f'Преподавателю все понравилось, можно приступать'
-                                                       f'к следующему уроку')
+                bot.send_message(chat_id=chat_id, text=dedent(f'''\
+                У вас проверили работу "{lesson_title}"
+                Преподавателю все понравилось, можно приступать
+                к следующему уроку'''))
             new_response = requests.get(
                 'https://dvmn.org/api/long_polling/',
                 headers=headers,
@@ -42,13 +45,15 @@ def get_reviews_status(auth_token, chat_id, bot):
             new_lesson_url = new_response.json()['new_attempts'][0]['lesson_url']
             new_review_status = new_response.json()['new_attempts'][0]['is_negative']
             if new_review_status:
-                bot.send_message(chat_id=chat_id, text=f'У вас проверили работу "{new_lesson_title}"\n'
-                                                       f'К сожалению, в работе нашлись ошибки\n'
-                                                       f'{new_lesson_url}')
+                bot.send_message(chat_id=chat_id, text=dedent(f'''\
+                У вас проверили работу "{new_lesson_title}"
+                К сожалению, в работе нашлись ошибки
+                {new_lesson_url}'''))
             else:
-                bot.send_message(chat_id=chat_id, text=f'У вас проверили работу "{new_lesson_title}"\n'
-                                                       f'Преподавателю все понравилось, можно приступать'
-                                                       f'к следующему уроку')
+                bot.send_message(chat_id=chat_id, text=dedent(f'''\
+                У вас проверили работу "{new_lesson_title}"
+                Преподавателю все понравилось, можно приступать
+                к следующему уроку'''))
         except requests.exceptions.ReadTimeout:
             logging.exception('Request timed out')
             continue
